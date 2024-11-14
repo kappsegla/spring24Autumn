a4e0af64f0cde55bc6b22d69640c6e830cedc4bf
Martin Blomberg <martin.blomberg@outlook.com>
2024-11-14 14:24:35 +0100

    Custom queries

--- src/main/java/org/example/spring24/apiauth/ApiKeyRepository.java
+++ src/main/java/org/example/spring24/apiauth/ApiKeyRepository.java
@@ -1,13 +1,22 @@
 package org.example.spring24.apiauth;
 
 import org.example.spring24.entity.ApiKey;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ApiKeyRepository extends ListCrudRepository<ApiKey,Integer> {
-    Optional<ApiKey> findByApiKey(String apiKey);
+public interface ApiKeyRepository extends ListCrudRepository<ApiKey, Integer> {
 
+    //JPQL language, Will be converted to database specific query in runtime
+    @Query("""
+            SELECT apiKey FROM ApiKey apiKey
+                        WHERE apiKey.name=:name
+            """)
     List<ApiKey> findApiKeysByName(String name);
+
+    //Native query, written as sql and for a specific database
+    @Query(value = "SELECT * FROM api_key where api_key =:apiKey", nativeQuery = true)
+    Optional<ApiKey> findByApiKey(String apiKey);
 }
