c55bb8ded108c93af428e3ced1a49722038d7e50
Martin Blomberg <martin.blomberg@outlook.com>
2024-11-16 20:47:48 +0100

    Add generic method for returning different projections

--- src/main/java/org/example/spring24/person/PersonRepository.java
+++ src/main/java/org/example/spring24/person/PersonRepository.java
@@ -7,18 +7,21 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 
+import java.util.Collection;
 import java.util.List;
 
 public interface PersonRepository extends ListCrudRepository<PersonEntity, Integer> {
 
     @Query("""
-       SELECT CONCAT(p.firstName, ' ', p.lastName) AS fullName, sm AS socialMedias
+
+            SELECT CONCAT(p.firstName, ' ', p.lastName) AS fullName, sm AS socialMedias
        FROM PersonEntity p
        LEFT JOIN p.socialMedias sm
        """)
     List<PersonWithSocialMediaProjection> findAllPersonsWithSocialMedia();
 
     @EntityGraph(attributePaths = {"socialMedias"})
-    List<PersonWithSocialMediaProjection> findBy(); // Automatically applies the projection.
+    List<PersonWithSocialMediaProjection> findBy(); // For the interface-based projection
 
+    <T> List<T> findBy(Class<T> type);
 }
