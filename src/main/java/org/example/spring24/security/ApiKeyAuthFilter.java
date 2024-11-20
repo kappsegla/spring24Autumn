7089a43ae65620f740a3e1733220ec32a7ee4143
Martin Blomberg <martin.blomberg@outlook.com>
2024-11-20 20:13:49 +0100

    Add possibility of anonymous auth with ApiKeyAuthFilter
     When no API key header is present the user will be set to anonymous so we can support permitAll or anonymous authorization. If a key is present but invalid we will return 401.

--- src/main/java/org/example/spring24/security/ApiKeyAuthFilter.java
+++ src/main/java/org/example/spring24/security/ApiKeyAuthFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.spring24.apiauth.ApiKeyAuthService;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -32,7 +33,7 @@ public class ApiKeyAuthFilter extends GenericFilterBean {
         try {
             Authentication authentication = getAuthentication((HttpServletRequest) request);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } catch (Exception exp) {
+        } catch (BadCredentialsException exp) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
@@ -40,18 +41,24 @@ public class ApiKeyAuthFilter extends GenericFilterBean {
             writer.print(exp.getMessage());
             writer.flush();
             writer.close();
+            return; // Stop further processing if authentication fails
         }
 
         chain.doFilter(request, response);
     }
 
 
+
     public Authentication getAuthentication(HttpServletRequest request) {
         String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        if (apiKey == null || apiKey.isEmpty()) {
+            return new AnonymousAuthenticationToken("anonymous", "anonymousUser", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+        }
         var name = apiKeyAuthService.isValidApiKey(apiKey);
         if (name.isEmpty()) {
             throw new BadCredentialsException("Invalid API Key");
         }
-        return new ApiKeyAuthentication(name.get(), AuthorityUtils.createAuthorityList("read:test","read:test"));
+        return new ApiKeyAuthentication(name.get(), AuthorityUtils.createAuthorityList("read:test", "read:test"));
     }
+
 }
